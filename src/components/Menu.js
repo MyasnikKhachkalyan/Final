cfe3fa2b684c424d23c11090e5ff43b0cffb6496
Myasnik <khachkalyan.myasnik@gmail.com>
2018-12-07 20:55:10 +0400

    little one

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,8 +7,8 @@ class Menu extends React.Component{
             <nav>
                 <ul className='menuul'>
                     <li className='menuli'><NavLink to='/home' className='menu'>Home</NavLink></li>
-                    <li className='menuli'><NavLink to='/game1' className='menu'>Game1</NavLink></li>
-                    <li className='menuli'><NavLink to='/game2' className='menu'>Game2</NavLink></li>
+                    <li className='menuli'><NavLink to='/game1' className='menu'>Puzzle</NavLink></li>
+                    <li className='menuli'><NavLink to='/game2' className='menu'>Quiz</NavLink></li>
                     <li className='menuli'><NavLink to='/years' className='menu'>30 Years</NavLink></li>
                     <li className='menuli'><NavLink to='/about' className='menu'>About Us</NavLink></li>
                 </ul>
